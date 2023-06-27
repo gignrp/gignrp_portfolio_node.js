a19387bfb206e5ec12573b7a0d5a5614c6f76fb9
gignrp <117824391+gignrp@users.noreply.github.com>
2023-06-27 17:29:31 +0200

    Add files via upload

---
+++ portfolio/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          web <code>GIGN RP</code> Frontend Developer.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/gignrp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GIGN RP
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
